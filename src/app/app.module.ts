4a384aa6f41cdf8f2e0ef68b6dd499952d506975
udaikiran <s.udaikiran@gmail.com>
2018-03-28 20:03:08 +0530

    router basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { AppComponent } from './app.component';
 import 'hammerjs';
+import { AppComponent } from './app.component';
+
 import { MenuComponent } from './menu/menu.component';
 import { CommentComponent } from './comment/comment.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
+import { DishService } from './services/dish.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
     declarations: [
@@ -21,13 +26,17 @@ import { FooterComponent } from './footer/footer.component';
         CommentComponent,
         DishdetailComponent,
         HeaderComponent,
-        FooterComponent
+        FooterComponent,
+        HomeComponent,
+        AboutComponent,
+        ContactComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         MaterialModule,
-        FlexLayoutModule
+        FlexLayoutModule,
+        AppRoutingModule
     ],
     providers: [DishService],
     bootstrap: [AppComponent]
